0937236011b96c314e46a790720edfcaf55dcb73
Hacker God <jyrone.parker@gmail.com>
2023-09-05 14:50:05 -0400

    url

--- routes/api.php
+++ routes/api.php
@@ -175,7 +175,7 @@ $video->dash()
 });
 
 Route::post('/stream-to-youtube', function(Request $request){
-  Log::info($request->all());
+  Log::info($request->url);
   $response = Http::attach(
     'file', $request->file
     )->put($request->url);
